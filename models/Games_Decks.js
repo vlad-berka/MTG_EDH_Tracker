a931d6d89269f58c4179a51172539f5fb8b802c2
Vlad Berka Desktop <vlad.m.berka@gmail.com>
2022-06-16 10:07:36 -0700

    Hotfix on circular dependency in model, just to make Heroku run

--- models/Games_Decks.js
+++ models/Games_Decks.js
@@ -6,23 +6,23 @@ class gamesDecks extends Model {}
 
 gamesDecks.init(
   {
-    // Unique ID for the category
-    game_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: Games,
-        key: "game_id",
-      },
-    },
-    deck_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: Decks,
-        key: "deck_id",
-      },
-    },
+    // // Unique ID for the category
+    // game_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: Games,
+    //     key: "game_id",
+    //   },
+    // },
+    // deck_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: Decks,
+    //     key: "deck_id",
+    //   },
+    // },
   },
   {
     sequelize,
