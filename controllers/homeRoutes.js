8e38e76a705b00daf10707a184dedf4836a09203
Vlad Berka Desktop <vlad.m.berka@gmail.com>
2022-06-16 10:12:23 -0700

    hotfix to make heroku work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,9 +6,10 @@ const { helper_function } = require("../utils/helpers");
 // GET for the homepage
 router.get("/", async (req, res) => {
   try {
-    await Games.sync();
-    await Decks.sync();
-    await gamesDecks.sync();
+    // await Games.sync();
+    // await Decks.sync();
+    // await gamesDecks.sync();
+
     //Find playgroup name
     //Hard coded for playgroup #1, but this should be req.params.id with /:id in the URL
     let play_id = 1;
@@ -22,27 +23,27 @@ router.get("/", async (req, res) => {
       return;
     }
 
-    const findGameHistory = await Games.findAll({
-      where: {
-        playgroup_id: 1
-      },
-      include: {
-        model: Decks,
-      }
-    });
+    // const findGameHistory = await Games.findAll({
+    //   where: {
+    //     playgroup_id: 1
+    //   },
+    //   include: {
+    //     model: Decks,
+    //   }
+    // });
 
     // const GameHistory = findGameHistory.get({plain: true});
-    var gameHistory = findGameHistory.map((game) => game.get({ plain: true }));
-    console.log("gameHistory is: ", gameHistory);
+    // var gameHistory = findGameHistory.map((game) => game.get({ plain: true }));
+    // console.log("gameHistory is: ", gameHistory);
 
-    if(!findGameHistory) {
-      res.status(404).json({message: `Error, no Gamehistory found for playgroup with id ${play_id}`});
-      return;
-    }
+    // if(!findGameHistory) {
+    //   res.status(404).json({message: `Error, no Gamehistory found for playgroup with id ${play_id}`});
+    //   return;
+    // }
 
     res.render("home-page", {
       playgroup,
-      gameHistory
+      // gameHistory
     });
   } catch (err) {
     console.log(err);
